f16b0a3226585de7df929dc20e50e1a004d3eca2
david <david146182003@gmail.com>
2025-07-11 21:00:37 -0400

    successfully get data

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,48 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Search from "./Search"
-const HomePage = ()=>{
+const HomePage = () => {
 
-    useEffect(()=>{
+    const [dataS, setData] = useState([])
+    
+    useEffect(() => {
         const apiKey = "4d05546a9ae849c28d065cd9bb27ec66"
         const url = `https://api.spoonacular.com/food/menuItems/search?query=burger&number=50&apiKey=${apiKey}`
-        async function getData(){
-            const response = await fetch(url);
-            const data = await response.json()
-            console.log(data)
+        async function getData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        const newData = []
+        for (let i = 0; i < data.number; i++) {
+            let img = data.menuItems[i].image;
+            let id = data.menuItems[i].id;
+            let restaurant = data.menuItems[i].restaurantChain;
+            let title = data.menuItems[i].title;
+            let dataObj ={
+                id: id,
+                restaurant: restaurant,
+                title: title,
+                img: img
+            }
+            newData.push(dataObj)
         }
-        getData()
-    },[])
+        setData(newData)
 
-    return(
+        console.log(dataS)
+
+    }
+        getData()
+    }, [])
+
+
+    return (
         <div>
+            {dataS.id}
             <Search />
+            <ul>
+                {dataS.map(title =>
+                    <li key={title.id}>{title.title}</li>
+                )}
+            </ul>
         </div>
     )
 }
